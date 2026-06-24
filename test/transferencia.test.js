602d3b0455aef3655389e7f8b5b1e8c93577e28d
Karina <ka.filipe@gmail.com>
2026-06-24 18:47:27 -0300

    Criei um helper de autenticação para obter o token

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -1,21 +1,12 @@
 const request = require('supertest');
 const { expect } = require('chai')
 require('dotenv').config()
+const { obterToken } = require ('../helpers/autenticacao')
 
 describe('Transferências', () => {
     describe('POST /transferencias', () => {
         it('Deve retornar sucesso com 201 quando o valor da transferência for igual ou acima de R$10,00', async () => {
-            //Capturar o token
-            const respostaLogin = await request(process.env.BASE_URL)
-                .post('/login')
-                .set('Content-Type', 'application/json')
-                .send({
-                    'username': 'julio.lima',
-                    'senha': '123456'
-                })
-            
-                
-            const token = respostaLogin.body.token
+            const token = await obterToken('julio.lima','123456')
 
             const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
@@ -35,17 +26,9 @@ describe('Transferências', () => {
         })        
 
         it('Deve retornar falha com 422 quando o valor da transferência for abaixo de R$10,00', async () => {
-            const respostaLogin = await request('http://localhost:3000')
-                .post('/login')
-                .set('Content-Type', 'application/json')
-                .send({
-                    'username': 'julio.lima',
-                    'senha': '123456'
-                })
-            
-            const token = respostaLogin.body.token
+            const token = await obterToken('julio.lima','123456')
 
-            const resposta = await request('http://localhost:3000')
+            const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Authorization', 'Bearer ' + token) //set('Authorization', `Bearer ${token}`) 
                 .set('Content-Type', 'application/json')
